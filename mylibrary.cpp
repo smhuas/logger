4d2a3ebdb4bd2967fb84f21bdc6f8f5a2e1404da
Patil Vijaya Bhaskar Reddy <emb-patired@embitel.com>
2024-03-09 18:00:47 +0530

    Initial commit

---
+++ mylibrary.cpp
@@ -0,0 +1,101 @@
+#include "mylibrary.hpp"
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 8080
+using namespace std;
+
+class singleton{
+public:
+    static string app_name;
+    static singleton *instance;
+    static std::ofstream outfile;
+    static singleton* getInstance(){
+        if(instance != nullptr)
+        {
+            instance = new singleton;
+            
+        }
+        return instance;
+    }
+};
+singleton* singleton::instance = nullptr;
+string singleton::app_name = "na";
+std::ofstream singleton::outfile(string("sample_log.txt"));
+
+string gettime(){
+    const auto now = std::chrono::system_clock::now();
+    const std::time_t t_c = std::chrono::system_clock::to_time_t(now);
+    string thetime = std::ctime(&t_c);
+    thetime.erase(thetime.find('\n', 0), 1);
+    return thetime;
+}
+
+bool registerLogger(string Appname)
+{
+    singleton::getInstance()->app_name = Appname;
+    return true;
+}
+
+void mylog(string type,string str)
+{
+    string logstring = gettime() + ": " + " " + "[App:" + singleton::getInstance()->app_name + "]" + " " + type + "\t: " + str;
+    cout<<logstring << endl;
+    singleton::getInstance()->outfile << logstring << endl;
+}
+
+int main()
+{
+    int server_fd, new_socket, valread;
+	struct sockaddr_in address;
+	int opt = 1;
+	int addrlen = sizeof(address);
+	char buffer[1024] = { 0 };
+	char* hello = "Hello from server";
+	
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
+	
+	if (setsockopt(server_fd, SOL_SOCKET,
+				SO_REUSEADDR | SO_REUSEPORT, &opt,
+				sizeof(opt))) {
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+	}
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(PORT);
+	
+	if (bind(server_fd, (struct sockaddr*)&address,
+			sizeof(address))
+		< 0) {
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+
+	while(1)
+	{
+		if (listen(server_fd, 3) < 0) {
+			perror("listen");
+			exit(EXIT_FAILURE);
+		}
+		if ((new_socket
+			= accept(server_fd, (struct sockaddr*)&address,
+					(socklen_t*)&addrlen))
+			< 0) {
+			perror("accept");
+			exit(EXIT_FAILURE);
+		}
+		valread = read(new_socket, buffer, 1024);
+        mylog("INFO",buffer);
+
+	}
+	
+	close(new_socket);
+	shutdown(server_fd, SHUT_RDWR);
+}
---
+++ mylibrary.hpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <fstream>  
+#include <chrono>
+using namespace std;
+
+void mylog(string type,string str);
+bool registerLogger(string Appname);
